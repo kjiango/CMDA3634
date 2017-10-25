3b8591e415c247eec3e56bc6979943c6d7385f0d
Kevin <kjiang@vt.edu>
2017-10-24 20:35:05 -0400

    added HW files

--- HW #3/network.c
+++ HW #3/network.c
@@ -31,7 +31,6 @@ typedef struct{
   // Destinations array pointers
   int* dest;
 
-
   // Book keeping for send and recieve buffers
   int* sendCount; // Size of send
   int* recvCount; // nuber of recvs the process is to expect
@@ -48,37 +47,123 @@ typedef struct{
 
 }Network;
 
-
+/**
+Network reader with parallel computing
+ */
 Network mpi_networkReader(char* filename){
-
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
+  Network g;
+  int i, j, temp;
+  FILE *fp;
+  int source;
+  int dest;
 
+  /*locNodes = g.nodes/size;
+  if((nodes%size) - 1 >= rank) {
+    locNodes++;   // determines number of nodes per processor
+    } */
   if(rank == 0){
     /* If root, read from data and distribute to all processors.
        Each processor should have global variables like number of nodes/max connects,
        every connection where one of its nodes are involved
        and the number of outbound connections of each of its nodes. See assignment
-       for spliting up node ownership and local indexing information.
+       for splitting up node ownership and local indexing information.
     */
+    int count = 0;
 
+    fp = fopen(filename, "r");
+    fscanf(fp, "%d,", &g.nodes);
+    temp = fscanf(fp, "%d,", &g.maxConnects);
+    //MPI_Bcast(&g.nodes,1,MPI_INT,0,MPI_COMM_WORLD);
+    //MPI_Bcast(&g.maxConnects,1,MPI_INT,0,MPI_COMM_WORLD);
+    
+    g.connectCount = (int*)calloc(g.nodes, sizeof(int));
+    g.source = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+    g.dest = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+    int *proc_source = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+    int *proc_dest = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+    // double *sendBuffer = 
 
+    g.sendOffset = (int*)malloc(sizeof(int));
+    g.recvOffset = (int*)malloc(sizeof(int));
+
+    do{
+      temp = fscanf(fp, "%d,%d,", &source, &dest);
+
+      if(temp == EOF) break;
+
+      g.source[count] = source;
+      g.dest[count] = dest;
+      g.connectCount[g.source[count]]++;
+
+      count++;
+    }while(temp != EOF);
+
+    for(i = 0; i < g.nodes; i++) {
+      int *sent = (int*)malloc(sizeof(int));
+      sent[0] = g.connectCount[i];
+      MPI_Send(sent,1,MPI_INT,i%size,1,MPI_COMM_WORLD);
+    }
+    for(i = 0; i < count; i++) {
+	proc_source[g.source[i]%size]++;
+	proc_dest[g.dest[i]%size]++;
+    }
+    //MPI_Bcast(proc_source,size,MPI_INT,0,MPI_COMM_WORLD);
+    //MPI_Bcast(proc_dest,size,MPI_INT,0,MPI_COMM_WORLD);
+
+    /*
+    for(i = 0; i < size; i++) {
+      int *sourceBuffer = (int*)malloc(proc_source[i] + sizeof(int));
+      int *destBuffer = (int*)malloc(proc_dest[i] + sizeof(int));
+
+      int temp1 = 0;
+      int temp2 = 0;
+
+      } */
+    fclose(fp);
+
+    if(rank != 0) {
+      MPI_Send(g.source, count, MPI_INT, rank, 1, MPI_COMM_WORLD);
+      MPI_Send(g.dest, count, MPI_INT, rank, 2, MPI_COMM_WORLD);
+      MPI_Send(g.connectCount, count, MPI_INT, rank, 3, MPI_COMM_WORLD);
+    }
+    int *local_source = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+    int *local_dest = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+
+    for(i = 0; i < count; i++) {
+      if(g.source[i]%size == g.dest[i]%size) {
+	local_source[i] = g.source[i];
+	local_dest[i] = g.dest[i];
+      }
+    }
+    
+    return g;
   }
   else{
-    /* Recieve data from root */
-
+    /* Receive data from root */
+    Network g;
+    int i;
+    MPI_Status status;
+    g.connectCount = (int*)calloc(g.nodes, sizeof(int));
+    g.source = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+    g.dest = (int*)calloc(g.nodes*g.maxConnects, sizeof(int));
+    
+    MPI_Recv(&g.source, 1, MPI_INT, rank, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&g.dest, 1, MPI_INT, rank, 2, MPI_COMM_WORLD, &status);
+    MPI_Recv(&g.connectCount, 1, MPI_INT, rank, 3, MPI_COMM_WORLD, &status);
   }
- 
 }
 
-
-
-/* Book keeping method. Tabulates counts and sets up data transfers */
-// Some order assumed: recv buffer contains chuncks of recv data in ascending rank order.                   
+/**
+Book keeping method. Tabulates counts and sets up data transfers 
+ */
+// Some order assumed: recv buffer contains chunks of recv data in ascending rank order.                   
 // That is, buffer looks like [Data from 0, Data from 1, ..., Data from n].                                 
 // It is assumed that the order source/destination data is stored is the same on all processors.            
-// This assumption saves us having to send companion destination data  
+// This assumption saves us having to send companion destination data
+
 Network mpi_tally(Network net){
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -99,36 +184,33 @@ Network mpi_tally(Network net){
     // Remember: (global node number) mod (number of processors) gives 
     // the processor the node belongs to. 
     sourceRank = source%size;
-    // Similar to compute which rank destiination belongs to
+    // Similar to compute which rank destination belongs to
     destRank = dest%size;
     if(sourceRank==rank && destRank!=rank){
       net.sendCount[destRank]++;
     }
     if(destRank==rank && sourceRank!=rank){
       net.recvCount[sourceRank]++;
-    }
-    
+    }   
   }
-
   // Count total number of send/recvs this processor needs to make
   net.totalSends = 0;
   net.totalRecvs = 0;
-  for(j=0; j<size; j++){
+  
+  for(j=0; j<size; j++) {
     net.totalSends += net.sendCount[j];
     net.totalRecvs += net.recvCount[j];
   }
-
   // Book keeping for sends and recevs
   // Sets offset markers to divide send/recv buffers
   // Effectivly reserves enough space for a buffer
   // to/from each processor
   net.sendOffset = (int*) calloc(size, sizeof(int));
   net.recvOffset = (int*) calloc(size, sizeof(int));
-  for(j=0;j<size-1;j++){
+  for(j=0;j<size-1;j++) {
     net.sendOffset[j+1] =  net.sendOffset[j]+net.sendCount[j];
     net.recvOffset[j+1] =  net.recvOffset[j]+net.recvCount[j];
   }
-
   // Book keeping for predetermining node destination of 
   // incoming data 
   net.recvLoc = (int*) calloc(net.totalRecvs, sizeof(int));
@@ -145,12 +227,13 @@ Network mpi_tally(Network net){
       count[sourceRank]++;
     }
   }
-
   return net;
-
+  
 }
 
-
+/**
+Updates the PageRank of each node using the PageRank algorithm
+ */
 Network  mpi_updatePageRank(Network net){
 
   // MPI variables
@@ -158,7 +241,7 @@ Network  mpi_updatePageRank(Network net){
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  // Varriables
+  // Variables
   int j, i, k;
   MPI_Status status;
   int tag = 999;
@@ -196,9 +279,7 @@ Network  mpi_updatePageRank(Network net){
 	count[destRank]++;	
       }
     }
-
   }
-
   marker = 0;
   // Send / Rcv
   // For all processors, loops over whose turn it is to send
@@ -209,8 +290,7 @@ Network  mpi_updatePageRank(Network net){
 	if(i!=j && net.sendCount[i] > 0){ // sendCount to self will be zero, no worries
 	  MPI_Send(sendBuffer+net.sendOffset[i], net.sendCount[i], MPI_DOUBLE, 
 		   i, tag, MPI_COMM_WORLD); 
-	}
-	
+	}	
       }
     }
     if(j!=rank && net.recvCount[j] > 0){
@@ -218,8 +298,7 @@ Network  mpi_updatePageRank(Network net){
       MPI_Recv(recvBuffer+marker, net.recvCount[j], MPI_DOUBLE, j, tag,
 	       MPI_COMM_WORLD, &status);
       marker+=net.recvCount[j]; // move marker so we dont overwrite data on next recv
-    }
-    
+    }   
   }
   int loc; // local node number of where recv data is going
   // Read through recved data and update PageRank of nodes the data belongs to
@@ -236,19 +315,32 @@ Network  mpi_updatePageRank(Network net){
   return net;
 }
 
-
-
+/**
+Computes the difference norm between the old and new PageRank arrays
+ */
 double mpi_computeDiff(Network net){
+  
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  /* Compute local sum of square differences and performa a reduction
+  /* Compute local sum of square differences and perform a reduction
      to sum over all processors, then take square root. 
   */
+  int i;
+  double local_diff = 0;
+  double global_diff;
 
+  for(i = 0; i < net.nodes; i++) {
+    local_diff += pow(net.newPageRank[i] - net.oldPageRank[i], 2);
+  }
+  // if(rank == 0) // MAYBE??
+  MPI_Allreduce(&local_diff, &global_diff, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  return sqrt(global_diff);
 }
 
-
+/**
+Updates the PageRank of each node using the PageRank algorithm
+ */
 Network mpi_computePageRank(Network net){
 
   double tol = 1e-6;
@@ -260,7 +352,6 @@ Network mpi_computePageRank(Network net){
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-
 
   /* Initialize old PR vlaues */
   for(k=0;k<net.locNodes;k++){
@@ -290,11 +381,13 @@ Network mpi_computePageRank(Network net){
     net.oldPageRank = net.newPageRank;
     net.newPageRank = temp;
   }
-
   return net;
+  
 }
 
-/* Free all (heap) arrays allocated. Make Valgrind happy */
+/**
+Free all (heap) arrays allocated. Make Valgrind happy 
+ */
 void networkDestructor(Network net){
   free(net.connectCount);
   free(net.source);
@@ -308,11 +401,11 @@ void networkDestructor(Network net){
   free(net.recvLoc);
 }
 
-
-
+/**
+Main Method
+ */
 int main(int argc, char** argv){
-
-
+  
   MPI_Init(&argc, &argv);
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -320,7 +413,6 @@ int main(int argc, char** argv){
 
   // Reads the filename of the data file
   char* filename = argv[1];
-
 
   //Start your code
   int j;
@@ -341,10 +433,10 @@ int main(int argc, char** argv){
      printf("PageRank of node %d is: %f\n", j, net.oldPageRank[j/size]);
    }
  }
- 
  // Program complete. Deconstruct network and finalize
  networkDestructor(net);
  MPI_Finalize();
 
  return 0;
+ 
 }
